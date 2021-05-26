5f765bdc43b804eb4e62f71bcbc72e6ce38b2aaa
Alex Cuenca M <maxpowerxd8y9@gmail.com>
2021-05-25 19:58:03 -0500

    Resolve line decoration on links

--- src/js/index.js
+++ src/js/index.js
@@ -1,4 +1,1 @@
-document.getElementsByClassName("navbar__button")[0].addEventListener("click",()=>{
-    const list = document.getElementsByClassName("navbar__list")[0];
-    list.classList.toggle("active");
-})
+console.log("hey");
